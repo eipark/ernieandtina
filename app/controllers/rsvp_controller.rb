b832145f8b9540c0512bc287186879b3c1e991b7
Ernie <ernestipark@gmail.com>
2013-04-21 23:17:43 -0400

    finish rsvp

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -8,6 +8,8 @@ class RsvpController < ApplicationController
 
     @guest = Guest.find_by_rsvp_code(@rsvp_code)
 
+    @plus_one = Guest.find_by_id(@guest.plus_one)
+
     if @guest
       render :template => 'rsvp/respond'
     else
@@ -20,6 +22,7 @@ class RsvpController < ApplicationController
   def complete
     @rsvp_code = params[:rsvp_code]
     @guest = Guest.find_by_rsvp_code(@rsvp_code)
+    @plus_one = Guest.find_by_id(@guest.plus_one)
 
     if !@guest
       flash[:error] = "Uh oh... something went wrong"
@@ -40,10 +43,31 @@ class RsvpController < ApplicationController
     @guest.comment = params[:comment]
     @guest.song_suggestion = params[:song_suggestion]
     @guest.allergies = params[:allergies]
+    @guest.food_choice = params[:food_choice]
 
     @guest.save
 
+    if @plus_one != nil
+      @po_attending = params[:po_attending]
+      if @po_attending == nil
+        flash[:error] = "Please select whether you will be attending or not."
+        redirect_to :back
+      elsif @po_attending == "yes"
+        @po_attending = true
+      else
+        @po_attending = false
+      end
+
+      @plus_one.rsvp = @po_attending
+      @plus_one.allergies = params[:po_allergies]
+      @plus_one.food_choice = params[:po_food_choice]
+
+      @plus_one.save
+
+    end
+
     render :template => 'rsvp/complete'
   end
+
 
 end
